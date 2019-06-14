74c3b619d9b4b2dc63a464786bcae2cab3fe9e05
avue7 <avue7@mail.csuchico.edu>
2019-06-13 17:55:42 -0700

    Adding background service.

--- src/android/HelloWorld.java
+++ src/android/HelloWorld.java
@@ -7,26 +7,78 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+// Testing background service
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 /**
  * This class echoes a string called from JavaScript.
  */
 public class HelloWorld extends CordovaPlugin {
 
-    @Override
-    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        if (action.equals("coolMethod")) {
-            String message = args.getString(0);
-            this.coolMethod(message, callbackContext);
+  Intent mServiceIntent;
+  private SensorService mSensorService;
+  Context ctx;
+
+  public Context getCtx() {
+    return ctx;
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    ctx = this;
+    setContentView(R.layout.activity_main);
+    mSensorService = new SensorService(getCtx());
+    mServiceIntent = new Intent(getCtx(), mSensorService.getClass());
+    if (!isMyServiceRunning(mSensorService.getClass())) {
+        startService(mServiceIntent);
+    }
+  }
+
+  private boolean isMyServiceRunning(Class<?> serviceClass) {
+    ActivityManager manager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+    for (ActivityManager.RunningServiceInfo service : manager.getRunningServices(Integer.MAX_VALUE)) {
+        if (serviceClass.getName().equals(service.service.getClassName())) {
+            Log.i ("isMyServiceRunning?", true+"");
             return true;
         }
-        return false;
     }
+    Log.i ("isMyServiceRunning?", false+"");
+    return false;
+  }
 
-    private void coolMethod(String message, CallbackContext callbackContext) {
-        if (message != null && message.length() > 0) {
-            callbackContext.success(message);
-        } else {
-            callbackContext.error("Expected one non-empty string argument.");
-        }
-    }
+
+  @Override
+  protected void onDestroy() {
+      stopService(mServiceIntent);
+      Log.i("MAINACT", "onDestroy!");
+      super.onDestroy();
+
+  }
+
+    // @Override
+    // public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+    //     if (action.equals("coolMethod")) {
+    //         String message = args.getString(0);
+    //         this.coolMethod(message, callbackContext);
+    //         return true;
+    //     }
+    //     return false;
+    // }
+    //
+    // private void coolMethod(String message, CallbackContext callbackContext) {
+    //     if (message != null && message.length() > 0) {
+    //         callbackContext.success(message);
+    //     } else {
+    //         callbackContext.error("Expected one non-empty string argument.");
+    //     }
+    // }
+
+
+
+
 }
